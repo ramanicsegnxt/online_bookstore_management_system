c8c7ebba5614a9b07e3e6980588d4a2a64e0f88b
Ramani Muniyappan <ramanicsegnxt@gmail.com>
2023-10-06 00:01:37 +0530

    spring boot project initial setup

---
+++ src/test/java/com/onlinestore/bookstoremanagementsystem/BookstoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore.bookstoremanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
